ea43cf4ac2ff559b40ec9f1d45da88e3794fb631
ranjbarmahdi <ranmahdi66@gmail.com>
2025-01-04 01:12:08 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({});
+
+const completion = openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    store: true,
+    messages: [{ role: 'user', content: 'write a haiku about ai' }],
+});
+
+completion.then((result) => console.log(result.choices[0].message));
